32e0af43370d5e06b4ef729f46ed618a18e55cd9
Mazhar05 <emailofsabuj@gmail.com>
2023-12-01 07:01:52 +0600

    I have added a custom js file

---
+++ src/custom.js
@@ -0,0 +1,5 @@
+
+const htmlTagSelect = document.getElementById('mainHtml');
+document.getElementById('dSwitchButton').addEventListener("click", function(){
+    htmlTagSelect.classList.toggle('dark')
+});
